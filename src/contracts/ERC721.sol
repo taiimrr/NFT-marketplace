5eb1776f64609247240b683f5fdb5af0366b4322
Taimoor Nasir <taimoornasir590@gmail.com>
2022-02-20 16:45:41 +0500

    transfer and approve functions

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -4,9 +4,11 @@ pragma solidity ^0.8.0;
 contract ERC721 {
 
 event Transfer(address indexed from, address indexed to,uint256 indexed tokenId);
+event Approval(address indexed owner , address indexed approved, uint256 indexed tokenId);
 
     mapping (uint256 => address) private _tokenOwner;
     mapping (address => uint256) private _ownerTokensCount;
+    mapping (uint256 => address) private _tokenApprovals;
 
 
     function balanceOf(address _owner) public view returns (uint256){
@@ -19,7 +21,7 @@ event Transfer(address indexed from, address indexed to,uint256 indexed tokenId)
         require(_tokenOwner[tokenId] != address(0));
         return _tokenOwner[tokenId];
     }
-    function _mint(address to, uint256 tokenId) internal {
+    function _mint(address to, uint256 tokenId) internal virtual {
         require(to != address(0), 'Insert a valid address');
         require(_tokenOwner[tokenId] == address(0),'Token Already minted');
         _tokenOwner[tokenId] = to;
@@ -27,4 +29,33 @@ event Transfer(address indexed from, address indexed to,uint256 indexed tokenId)
 
         emit Transfer(address(0), to, tokenId);
     }
+
+     function _transferFrom(address _from, address _to, uint256 _tokenId) internal{
+        require(_to != address(0), 'Insert a valid address');
+        require(ownerOf(_tokenId) == _from,"Onwer address is incorrect!!");
+
+        _tokenOwner[_tokenId] = _to;
+        _ownerTokensCount[_from] -= 1; 
+        _ownerTokensCount[_to] += 1; 
+
+         emit Transfer(_from, _to, _tokenId);
+
+
+
+    }
+
+    function transferFrom(address _from, address _to, uint256 _tokenId) public {
+        _transferFrom( _from,  _to,  _tokenId);
+    }
+
+    function approve(address _to, uint256 tokenId) public{
+        address owner = ownerOf(tokenId);
+        require(_to != owner,"Error: Approval to same address!");
+        require(owner == msg.sender,"You must be owner to approve this!!");      
+        _tokenApprovals[tokenId] = _to;  
+
+        emit Approval(owner, _to, tokenId);
+
+    }
+
 }
---
+++ src/contracts/ERC721Enumerable.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import './ERC721.sol';
+
+contract ERC721Enumerable is ERC721 {
+
+    uint[] private _allTokens; 
+    mapping (uint256 => uint256) private _allTokenIndex;
+    mapping (address => uint256[]) private ownerTokenIds;
+    mapping (uint256 => uint256) private _ownedTokensIndex;
+
+
+
+    function totalSupply() external view returns (uint256){
+        return _allTokens.length;
+    }
+
+    function _mint(address to, uint256 tokenId) internal override(ERC721){
+            super._mint(to,tokenId);
+
+            addTokensToAllTokenEnumeration(tokenId);
+            addTokensToOwnerEnumeration(to,tokenId);
+    }
+
+    function tokenByIndex(uint256 index) view public returns(uint256){
+        require(index < this.totalSupply(), 'Index out of bounds!');
+        return _allTokens[index];
+    }
+    function tokenOfOwnerByIndex(address owner,uint256 index) view public returns(uint256){
+        require(index < this.balanceOf(owner), 'Index out of bounds!');
+        return ownerTokenIds[owner][index];
+    }
+
+    function addTokensToAllTokenEnumeration(uint256 tokenId ) private{
+        _allTokenIndex[tokenId] = _allTokens.length;
+        _allTokens.push(tokenId);
+    }
+
+    function addTokensToOwnerEnumeration(address to, uint256 tokenId) private{
+        _ownedTokensIndex[tokenId] = ownerTokenIds[to].length;
+        ownerTokenIds[to].push(tokenId);
+
+    }
+}
